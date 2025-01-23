5fc636186324e13bc2756fc95b0bff11d2fbaab5
Mok <2576569975@qq.com>
2025-01-23 16:13:35 +0800

    style uploadFile

--- lib/widgets/upload_file.dart
+++ lib/widgets/upload_file.dart
@@ -31,74 +31,86 @@ class UploadFile extends StatelessWidget {
             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 8),
-          if (onUpload != null)
-            GestureDetector(
-              onTap: onUpload,
-              child: Container(
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.upload, color: AppColors.primaryColor),
-                        SizedBox(width: 8),
-                        Text('Click to upload',
+          Container(
+            color: AppColors.backgroundColor,
+            child: Column(
+              children: [
+                if (onUpload != null)
+                  GestureDetector(
+                    onTap: onUpload,
+                    child: Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.all(8),
+                      padding: EdgeInsets.fromLTRB(16, 28, 16, 24),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.upload, color: AppColors.primaryColor),
+                              SizedBox(width: 8),
+                              Text('Click to upload',
+                                  style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'supporting formats: Word, Excel, PDF, JPG, JPEG, PNG, ZIP, RAR, under 20MB',
                             style: TextStyle(
-                                color: AppColors.primaryColor,
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      'supporting formats: Word, Excel, PDF, JPG, JPEG, PNG, ZIP, RAR, under 20MB',
-                      style: TextStyle(
-                          color: AppColors.auxiliaryColor, fontSize: 15),
-                      textAlign: TextAlign.center,
-                    )
-                  ],
-                ),
-              ),
-            ),
-          SizedBox(height: 16),
-          if (uploadedFiles.isNotEmpty)
-            Column(
-              children: uploadedFiles.map((file) {
-                return ListTile(
-                  leading: Icon(
-                    Icons.picture_as_pdf_rounded,
-                    color: Colors.deepOrangeAccent,
-                  ),
-                  title: Text(file),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      if (onDownload != null)
-                        IconButton(
-                          icon: Icon(Icons.download, color: Colors.blue),
-                          onPressed: () {
-                            onDownload!(file); // 触发下载回调
-                          },
-                        ),
-                      IconButton(
-                        icon: Icon(Icons.delete, color: Colors.red),
-                        onPressed: () {
-                          if (onDelete != null) {
-                            onDelete!(file); // 触发删除回调
-                          }
-                        },
+                                color: AppColors.auxiliaryColor, fontSize: 15),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                );
-              }).toList(),
+                if (uploadedFiles.isNotEmpty)
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    child: Column(
+                      children: uploadedFiles.map((file) {
+                        return Container(
+                          color: Colors.white,
+                          margin: EdgeInsets.all(8), // 为每个 ListTile 添加外边距
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.picture_as_pdf_rounded,
+                              color: Colors.deepOrangeAccent,
+                            ),
+                            title: Text(file),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                if (onDownload != null)
+                                  IconButton(
+                                    icon: Icon(Icons.download,
+                                        color: Colors.blue),
+                                    onPressed: () {
+                                      onDownload!(file); // 触发下载回调
+                                    },
+                                  ),
+                                IconButton(
+                                  icon: Icon(Icons.delete, color: Colors.red),
+                                  onPressed: () {
+                                    if (onDelete != null) {
+                                      onDelete!(file); // 触发删除回调
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+              ],
             ),
+          ),
         ],
       ),
     );
